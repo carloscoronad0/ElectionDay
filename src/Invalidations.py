f8c7e69aa5ee8f519acec059994796e6decfbff4
EC2 Default User <ec2-user@ip-172-31-43-5.us-east-2.compute.internal>
2021-06-29 06:26:53 +0000

    <ED-04><Basics of invalidateBooth and showInvalidBooths completed>

--- src/Invalidations.py
+++ src/Invalidations.py
@@ -1,6 +1,7 @@
 import json
 import boto3
 import os
+from boto3.dynamodb.conditions import Key
 
 electionDay_table = os.environ['ELECTIONDAY_TABLE']
 
@@ -10,17 +11,48 @@ table = dynamodb.Table(electionDay_table)
 def invalidateBooth(event, context):
     print(json.dumps(event))
     
+    path = event["path"]
+    array_path = path.split("/")
+    booth_id = array_path[-2]
+    
+    body = event["body"]
+    body_object = json.loads(body)
+    
+    table.put_item(
+       Item = {
+            'pk': 'Inv_' + body_object['city_id'],
+            'sk': booth_id,
+            'school': body_object['school_id']
+        }
+    )
+    
     return {
         'statusCode': 200,
-        'body': json.dumps("InvalidateBooth")
+        'body': json.dumps(f"Invalidated booth {booth_id}")
     }
     
 def showInvalidBooths(event, context):
     print(json.dumps(event))
     
+    cities = table.query(
+        KeyConditionExpression = Key('pk').eq('CityReg')
+    )
+    
+    cities_items= cities['Items']
+    print(json.dumps(cities_items))
+    
+    booths = []
+    
+    for eachcity in cities_items:
+        aux = 'Inv_' + eachcity['id']
+        temp = table.query(
+            KeyConditionExpression = Key('pk').eq(aux)
+        )
+        booths.extend(temp['Items'])
+    
     return {
         'statusCode': 200,
-        'body': json.dumps("ShowInvalidBooths")
+        'body': json.dumps(booths)
     }
     
 def showInvalidBoothsByCity(event, context):
