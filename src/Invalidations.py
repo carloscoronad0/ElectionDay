d0f9c32bd237fe1b21c31aa3c7e234b62ff42d02
EC2 Default User <ec2-user@ip-172-31-43-5.us-east-2.compute.internal>
2021-06-30 10:49:41 +0000

    <ED-04><Finished first versions of lambda functions>

--- src/Invalidations.py
+++ src/Invalidations.py
@@ -2,28 +2,64 @@ import json
 import boto3
 import os
 from boto3.dynamodb.conditions import Key
+from decimal import Decimal
 
 electionDay_table = os.environ['ELECTIONDAY_TABLE']
 
+client = boto3.client('dynamodb')
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(electionDay_table)
 
 def invalidateBooth(event, context):
-    print(json.dumps(event))
-    
     path = event["path"]
     array_path = path.split("/")
     booth_id = array_path[-2]
     
-    body = event["body"]
-    body_object = json.loads(body)
+    body_temp = event["body"]
+    body = json.loads(body_temp)
     
-    table.put_item(
-       Item = {
-            'pk': 'Inv_' + body_object['city_id'],
-            'sk': booth_id,
-            'school': body_object['school_id']
+    invalid_booth = table.get_item(
+        Key = {
+            'pk': 'Sch_' + body['school_id'],
+            'sk': booth_id
         }
+    )
+    
+    newPkPut = 'Inv_' + body['city_id']
+    newPkUpdate = 'Av_' + body['city_id']
+    school = body['school_id']
+    
+    val = str(invalid_booth['Item']['missing'])
+    
+    response = client.transact_write_items(
+        TransactItems=[
+            {
+                'Put': {
+                    'TableName': 'election-day-table',
+                    'Item': {
+                        'pk': { 'S': newPkPut },
+                        'sk': { 'S': booth_id },
+                        'school': { 'S': school }
+                    }
+                }
+            },
+            {
+                'Update': {
+                    'TableName': 'election-day-table',
+                    'Key': {
+                        'pk': { 'S': newPkUpdate },
+                        'sk': { 'S': school }
+                    },
+                    'UpdateExpression': 'SET #miss = #miss - :val',
+                    'ExpressionAttributeNames': {
+                        '#miss': 'missing'
+                    },
+                    'ExpressionAttributeValues': {
+                      ':val': { 'N': val }
+                    }
+                }
+            }
+        ]
     )
     
     return {
@@ -32,23 +68,35 @@ def invalidateBooth(event, context):
     }
     
 def showInvalidBooths(event, context):
-    print(json.dumps(event))
-    
     cities = table.query(
         KeyConditionExpression = Key('pk').eq('CityReg')
     )
     
     cities_items= cities['Items']
-    print(json.dumps(cities_items))
     
-    booths = []
-    
+    invalids = []
     for eachcity in cities_items:
         aux = 'Inv_' + eachcity['id']
         temp = table.query(
             KeyConditionExpression = Key('pk').eq(aux)
         )
-        booths.extend(temp['Items'])
+        invalids.extend(temp['Items'])
+        
+    booths = []
+    for eachbooth in invalids:
+        temp = table.get_item(
+            Key = {
+                'pk': 'Sch_' + eachbooth['school'],
+                'sk': eachbooth['sk']
+            }
+        )
+        item = {
+            "booth_id": temp['Item']['sk'],
+            "registered": str(temp['Item']['registered']),
+            "missing": str(temp['Item']['missing']),
+            "voted": str(temp['Item']['voted'])
+        }
+        booths.append(item)
     
     return {
         'statusCode': 200,
@@ -56,17 +104,71 @@ def showInvalidBooths(event, context):
     }
     
 def showInvalidBoothsByCity(event, context):
-    print(json.dumps(event))
+    cities = table.query(
+        KeyConditionExpression = Key('pk').eq('CityReg')
+    )
     
+    cities_items= cities['Items']
+    
+    invalids = []
+    for eachcity in cities_items:
+        aux = 'Inv_' + eachcity['id']
+        temp = table.query(
+            KeyConditionExpression = Key('pk').eq(aux)
+        )
+        item={
+            "city_id": eachcity['id'],
+            "city_name": eachcity['sk'],
+            "invalids_count": str(temp['Count'])
+        }
+        invalids.append(item)
+    
+    invalids.sort(reverse=True, key=criteria)
+    
+    print(json.dumps(invalids))
     return {
         'statusCode': 200,
-        'body': json.dumps("ShowInvalidBoothsByCity")
+        'body': json.dumps(invalids)
     }
     
 def showInvalidBoothsBySchool(event, context):
     print(json.dumps(event))
     
+    path = event["path"]
+    array_path = path.split("/")
+    city_id = array_path[-2]
+    
+    aux = f"Av_{city_id}"
+    schools = table.query(
+        KeyConditionExpression = Key('pk').eq(aux)
+    )
+    
+    invalids = []
+    for eachschool in schools['Items']:
+        invCity = f"Inv_{city_id}"
+        temp = eachschool['sk']
+        booths = table.query(
+            KeyConditionExpression = Key('pk').eq(invCity),
+            FilterExpression = "school = :sch",
+            ExpressionAttributeValues={
+                ':sch': temp
+            }
+        )
+        
+        item={
+            "school_id": eachschool['sk'],
+            "city_id": city_id,
+            "invalids_count": str(temp['Count'])
+        }
+        invalids.append(item)
+        
+    invalids.sort(reverse=True, key=criteria)
+    
+    print(json.dumps(invalids))
     return {
         'statusCode': 200,
-        'body': json.dumps("ShowInvalidBoothsBySchool")
+        'body': json.dumps(invalids)
     }
+    
+def criteria(e):
+    return Decimal(e["invalids_count"])
